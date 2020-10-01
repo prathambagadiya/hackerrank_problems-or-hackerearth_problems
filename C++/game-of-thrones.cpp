a49b26cbc0cd7319fd6ee0e17a64bf3feb77fc20
ankitagatiyala <72201422+ankitagatiyala@users.noreply.github.com>
2020-10-01 16:49:25 +0530

    Create game-of-thrones.cpp

---
+++ C++/game-of-thrones.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the gameOfThrones function below.
+string gameOfThrones(string s) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = gameOfThrones(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
