69ec00d02b72d93fa8e176203c6a73405d51a938
divyanshugatiyala <91906800+divyanshugatiyala@users.noreply.github.com>
2021-10-04 22:31:02 +0530

    Create Let and Const.js

---
+++ JavaScript/Let and Const.js
@@ -0,0 +1,13 @@
+
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r = readLine();
+    const PI= Math.PI;
+    console.log(PI*(r**2));
+    console.log(2*PI*r);
+
+    // Print the area of the circle:
+
+    // Print the perimeter of the circle:
+
