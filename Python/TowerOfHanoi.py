2a8408b8cfac4ff4fc9c84c735c43dc9943676b1
HarshithSimha <harshithgsimha@gmail.com>
2022-09-27 13:22:46 +0530

    Added TowerOfHanoi.py file
    Signed-off-by: HarshithSimha <harshithgsimha@gmail.com>

--- Python/TowerOfHanoi.py
+++ Python/TowerOfHanoi.py
@@ -4,14 +4,14 @@
 # TOH = Tower Of Hanoi
 
 def TOH(n, s, d, a):
-	if n==1:
-		print ("Move disk 1 from source",s,"to destination",d)
-		return
-	TOH(n-1, s, a, d)
+    if n==1:
+        print ("Move disk 1 from source",s,"to destination",d)
+        return
+    TOH(n-1, s, a, d)
 
-	print ("Move disk",n,"from source",s,"to destination",d)
-	TOH(n-1, a, d, s)
-		
+    print ("Move disk",n,"from source",s,"to destination",d)
+    TOH(n-1, a, d, s)
+
 # Driver code
 n = 4
 TOH(n,'A','B','C')
